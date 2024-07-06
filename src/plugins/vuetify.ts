971901973b47834c15d356f194e035e15e57f881
Inês Soares <inesosoares.6@gmail.com>
2024-07-06 16:46:46 +0100

    change theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -8,12 +8,38 @@
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
+import colors from 'vuetify/util/colors'
+
 // Composables
 import { createVuetify } from 'vuetify'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
 	theme: {
-		defaultTheme: 'dark'
+		defaultTheme: 'dark',
+		themes: {
+			light: {
+				dark: false,
+				colors: {
+					primary: colors.orange.lighten1,
+					secondary: colors.cyan.lighten3,
+					error: colors.orange.lighten3,
+					grey: colors.grey.lighten2,
+					warning: colors.yellow.lighten2,
+					background: colors.grey.lighten5
+				}
+			},
+			dark: {
+				dark: true,
+				colors: {
+					primary: colors.orange.lighten1,
+					secondary: colors.cyan.lighten3,
+					error: colors.orange.lighten3,
+					grey: colors.grey.lighten2,
+					warning: colors.yellow.lighten2,
+					background: colors.grey.darken4
+				}
+			}
+		}
 	}
 })
